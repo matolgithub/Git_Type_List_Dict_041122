3c8c816535286a82d929878a272d2c2205e67376
matolgithub <89175955650@mail.ru>
2022-11-05 00:19:24 +0300

    Final commit, the end of project.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from prepaire_for_main import Data
 
 
 def main():
-    Data("text_file.txt").sort_data()
+    Data("text_file.txt").write_output_files()
 
 
 if __name__ == "__main__":
--- prepaire_for_main.py
+++ prepaire_for_main.py
@@ -5,20 +5,20 @@ class Data:
     def __init__(self, file: str):
         self.file = file
 
-    def read_data(self):
+    def read_data(self) -> list:
         url_file = f"input_data/{self.file}"
         with open(file=url_file, encoding="utf-8") as open_file:
             list_total_data = open_file.read().split(" ")
 
         return list_total_data
 
-    def clear_list(self):
+    def clear_list(self) -> list:
         list_total_data = Data(self.file).read_data()
         clear_list_data = [item for item in list_total_data if item != ""]
 
         return clear_list_data
 
-    def sort_data(self):
+    def sort_data(self) -> dict:
         clear_list_data = Data(self.file).clear_list()
         sort_dict_data = {"digits": [], "names": [], "patronymics": [], "surnames": []}
         for item in clear_list_data:
@@ -31,6 +31,19 @@ class Data:
                     sort_dict_data["surnames"].append(item)
                 else:
                     sort_dict_data["names"].append(item)
-        print(sort_dict_data)
 
         return sort_dict_data
+
+    def write_output_files(self):
+        sort_dict_data = Data(self.file).sort_data()
+        for key, item in sort_dict_data.items():
+            try:
+                with open(f"output_data/{key}.txt", "w", encoding="utf-8") as output_file:
+                    output_file.write(str(item))
+                    print(f"Write: {key}.txt. Context: {item}.")
+                status_result = "Success"
+            except FileNotFoundError:
+                FileNotFoundError.strerror
+                status_result = "Error"
+            finally:
+                print(f"The end of process with status: {status_result}.")
